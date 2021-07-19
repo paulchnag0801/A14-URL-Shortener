10f78f9fbae324c19bbeb3cf7b3e310f1e1181b0
paulchnag0801 <x1232178987@gmail.com>
2021-07-19 18:12:03 +0800

    feat: add code comments

--- public/javascripts/copyUrl.js
+++ public/javascripts/copyUrl.js
@@ -1,17 +1,20 @@
+// init variables
 const copyUrl = document.querySelector('.short-url')
 const copyBtn = document.querySelector('.copy-button')
 const toolTip = document.querySelector('.tool-tip')
 
+// copy function
 if (copyUrl) {
-  copyBtn.addEventListener('click', function btnOnClicked () {
-    copyUrl.select()
+  copyBtn.addEventListener('click', function btnOnClicked() {
+    copyUrl.select() // select short URL
     if (document.execCommand('copy')) {
-      toolTip.classList.add('show')
-      setTimeout(function removeToolTip () {
+      // copy short URL to clipboard
+      toolTip.classList.add('show') // show tool tip
+      setTimeout(function removeToolTip() {
         toolTip.classList.remove('show')
       }, 700)
     } else {
-      console.log("Umm..Seems like your browser doesn't support js to copy to clipboard...")
+      console.log('Umm.. Something is wrong...')
     }
   })
 }
